e8ff34d8ec462844360350a309b2b31438416088
pratham534 <prathamsrivastava534@gmail.com>
2023-06-12 21:02:14 +0530

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react"
+// import trollface from 
+
+export default function Header() {
+    return (
+        <header className="header">
+            <img 
+                src={require("../images/troll-face.png") }
+                className="header--image"
+            />
+            <h2 className="header--title">Meme Generator</h2>
+        </header>
+    )
+}
